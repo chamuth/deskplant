31e8e74f2ceb5b0e27fdabe41ccb53d2e8719931
Chamuth Chamandana <c.chamandana@gmail.com>
2020-04-04 12:04:58 +0530

    routes changed

--- routes/web.php
+++ routes/web.php
@@ -28,11 +28,16 @@ Route::post('order', 'OrderController@place')->name('order.place');
 
 Route::get('order/{order}', 'OrderController@successful')->name('order.successful');
 
-Route::get('admin/dashboard', 'OrdersDashboardController@show')->name('dashboard.show');
-Route::get('admin/dashboard/orders', 'OrdersDashboardController@showOrders')->name('dashboard.showorders');
 
-Route::post("api/units", "OrdersDashboardController@updateUnits")->name('dashboard.api.units');
-Route::post("api/delivery_method", "OrdersDashboardController@updateDeliveryMethods")->name('dashboard.api.deliverymethod');
+Route::middleware(['admin.auth:admin', 'permission'])
+->group(function()
+{
+    Route::get('admin/dashboard', 'OrdersDashboardController@show')->name('dashboard.show');
+    Route::get('admin/dashboard/orders', 'OrdersDashboardController@showOrders')->name('dashboard.showorders');
+
+    Route::post("api/units", "OrdersDashboardController@updateUnits")->name('dashboard.api.units');
+    Route::post("api/delivery_method", "OrdersDashboardController@updateDeliveryMethods")->name('dashboard.api.deliverymethod');
+});
 
 Route::middleware('auth')
     ->name('account.')
